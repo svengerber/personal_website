d2fa8f1c944c2e5662f57d9922a4395e35514428
Sven Gerber <github@gerber.uno>
2022-11-27 01:04:01 +0100

    Adding md rendering and material ui

---
+++ components/layout.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Welcome to my personal website
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <main>{children}</main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,67 @@
-function HomePage() {
-    return <div>Welcome to my blog</div>
-  }
-  
-  export default HomePage
+import fs from 'fs';
+import matter from 'gray-matter';
+import Image from 'next/image';
+import Link from 'next/link';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+
+
+export async function getStaticProps() {
+  const files = fs.readdirSync('posts');
+
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data: frontmatter } = matter(readFile);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+const Item = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center'
+}));
+
+export default function Home({ posts }) {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+      {posts.map(({ slug, frontmatter }) => (
+        
+        <div key={slug}>
+          <Grid xs={8}>
+            <Item>
+            <Link href={`/post/${slug}`} legacyBehavior>
+            <a>
+              <Image
+                width={540}
+                height={300}
+                alt={frontmatter.title}
+                src={`/${frontmatter.coverImage}`}
+              />
+              <h1>{frontmatter.title}</h1>
+            </a>
+          </Link>
+          </Item>
+          </Grid>
+
+        </div>
+      ))}
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
