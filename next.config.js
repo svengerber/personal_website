365ef0fd0f7e7f85f7121be40a358c2a34488414
Sven Gerber <github@gerber.uno>
2022-11-27 01:37:01 +0100

    edit nextjs config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-//default values
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}`
-  basePath = `/${repo}`
-
-  module.exports = {
-    assetPrefix: assetPrefix,
-    basePath: basePath,
+module.exports = {
     images: {
         unoptimized: true,
     },
-  }
-
-}
-else {
-    //export undefined if not on ci
-    module.exports = {
-        assetPrefix: undefined,
-        basePath: undefined,
-        images: {
-            unoptimized: true,
-        },
-      }
 }
