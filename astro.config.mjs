f7631bcc31f128f71974695dc970b92497cd49a3
Sven Gerber <github@gerber.uno>
2023-01-15 00:06:09 +0100

    custom welcome message

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,33 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+//Frameworks
+import svelte from "@astrojs/svelte";
+
 // Hosting URL Config 
 const SERVER_PORT = 3000;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_SITE = "https://svengerber.github.io";
-
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-
 let BASE_SITE = LOCALHOST_URL;
-let BASE_PATH = "/personal_website" //GitHub Repo Name  // Clear if custom domain
+let BASE_PATH = "/personal_website"; //GitHub Repo Name  // Clear if custom domain
 
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_SITE = LIVE_SITE;
 }
 
+// https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
+  server: {
+    port: SERVER_PORT
+  },
   site: BASE_SITE,
   base: BASE_PATH,
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,12 @@
+<script>
+    const messages = ["Hi", "Hello", "Hola", "Hey there","Welcome"]
+    const greeting = messages[(Math.floor(Math.random() * messages.length))];
+</script>
+
+<div>
+<span
+class="bg-gradient-to-br from-primary-400 via-primary-500 to-primary-600 dark:from-primary-main dark:via-primary-main dark:to-primary-main bg-clip-text text-transparent"
+>
+{greeting}!
+</span>
+</div>
