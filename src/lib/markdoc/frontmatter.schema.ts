798063eff796c33547965af111655ccf01f656df
Sven Gerber <github@gerber.uno>
2023-01-15 13:17:43 +0100

    remove project tab and components

--- src/lib/markdoc/frontmatter.schema.ts
+++ src/lib/markdoc/frontmatter.schema.ts
@@ -45,17 +45,3 @@ export const blog = z.discriminatedUnion("external", [
   blogMarkdown,
   blogExternal,
 ]);
-
-export const project = z.object({
-  draft: z.boolean().default(false),
-  title: z.string({
-    required_error: "Required frontmatter missing: title",
-    invalid_type_error: "title must be a string",
-  }),
-  date: z.date({
-    required_error: "Required frontmatter missing: date",
-    invalid_type_error:
-      "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
-  }),
-  url: z.string(),
-});
