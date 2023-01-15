f3b4d50bb7e3bd06a34e40284445706bf0c5524e
Sven Gerber <github@gerber.uno>
2023-01-15 12:36:11 +0100

    basic tag index and tag page

--- content/blog/draft-post.md
+++ content/blog/draft-post.md
@@ -4,7 +4,7 @@ draft: true
 title: Draft Post
 description: This post is a draft and won't be built.
 date: 2022-11-22
-tags: ["demo"]
+tags: ["demo","short"]
 ---
 
 It's a beautiful world out there.
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Sven Gerber";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Welcome to my blog! I write about system and cloud engineering.";
+export const TWITTER_HANDLE = "@mytwitterhandle";
+export const MY_NAME = "Sven Gerber";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
